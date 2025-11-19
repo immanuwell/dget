09a3481ae8c9d8dfa62a05be90547bb00bf4af6f
immanuwell <pchpr.00@list.ru>
2025-11-19 12:58:53 +0400

    feat: enhanced this test

--- dget_test.go
+++ dget_test.go
@@ -4,22 +4,38 @@ import (
 	"os/exec"
 	"strings"
 	"testing"
+	"fmt"
 )
 
+type CmdOutput struct {
+	Cmd    *exec.Cmd
+	Output string
+}
+
 func TestCommandOutput(t *testing.T) {
-	cmd := exec.Command("go", "run", ".", "--base", "example,test", "--tld", "com,net", "--debug")
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("command failed: %v\noutput:\n%s", err, out)
+	commands := []CmdOutput{
+		{Cmd: exec.Command("go", "run", ".", "--debug", "example.com"),
+			Output: "[example.com]"},
+		{Cmd: exec.Command("go", "run", ".", "--base", "example,test", "--tld", "com,net", "--debug"),
+			Output: "[example.com example.net test.com test.net]"},
+		{Cmd: exec.Command("go", "run", ".", "--base", "example,test", "--tld", "com", "--tld", "net", "--debug"),
+			Output: "[example.com example.net test.com test.net]"},
 	}
 
-	output := strings.TrimSpace(string(out)) // remove trailing \n
-	lines := strings.Split(output, "\n")     // split into lines
-	got := lines[len(lines)-1]               // last printed line
-	want := "[example.com example.net test.com test.net]"
+	for _, item := range commands {
+		out, err := item.Cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("[%s] error: %v\n", item.Output, err)
+			continue
+		}
+		want := item.Output
+		output := strings.TrimSpace(string(out)) // remove trailing \n
+		lines := strings.Split(output, "\n")     // split into lines
+		got := lines[len(lines)-1]               // last printed line
 
-	if got != want {
-		t.Fatalf("unexpected output:\n got: %q\nwant: %q", got, want)
+		if got != want {
+			t.Fatalf("unexpected output:\n got: %q\nwant: %q", got, want)
+		}
 	}
+
 }
